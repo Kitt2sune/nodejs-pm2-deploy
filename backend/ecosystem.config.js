c6404e3a0e1a706521d2502e8ba0907803a35c0a
Kitt2sune <kitt2sune@gmail.com>
2026-02-05 21:52:04 +0300

    Feat: add dependenecies

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy': `scp -i ${DEPLOY_SSH_KEY} .env .env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/backend/`,
-      'post-deploy': 'cd backend && . ~/.nvm/nvm.sh && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production'
+      'post-deploy': 'cd backend && . ~/.nvm/nvm.sh && npm i && rm -rf dist && npm run build && pm2 startOrRestart ecosystem.config.js --env production'
     },
   },
 };
